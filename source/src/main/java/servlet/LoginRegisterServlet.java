4bdf1a40849de79053cb984f891d63182fa9db58
plusdojo2026-watanabe-r <watanabe-ryutaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-10 17:06:43 +0900

    追加

--- source/src/main/java/servlet/LoginRegisterServlet.java
+++ source/src/main/java/servlet/LoginRegisterServlet.java
@@ -2,12 +2,15 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.naming.spi.DirStateFactory.Result;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.CommonDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -38,6 +41,18 @@ public class LoginRegisterServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		// 登録処理
+		CommonDAO Dao = new CommonDAO();
+		if(Dao.insert(new Common(id, password))) {
+			request.setAttribute("result", new Result("登録完了", "ユーザーIDとパスワードを登録しました。", "/e3/MenuServlet"));
+		} else {
+			// 登録失敗
+			request.setAttribute("result", new Result("登録失敗", "登録できませんでした。", ""));
+		}
 	}
 
 }
--- source/src/main/webapp/WEB-INF/jsp/loginRegister.jsp
+++ source/src/main/webapp/WEB-INF/jsp/loginRegister.jsp
@@ -5,7 +5,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>モノカチコレクション | 新規登録</title>
 </head>
 <body>
 
