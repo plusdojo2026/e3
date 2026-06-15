310114b010901c460059ec03c799ec3e57eff424
plusdojo2026-ishidareoto <ishida-reoto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 18:01:39 +0900

    最新版です

--- source/src/main/java/servlet/RegisterServlet.java
+++ source/src/main/java/servlet/RegisterServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/RegisterServlet")
+@MultipartConfig
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -36,10 +38,12 @@ public class RegisterServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	@Override
+	protected void doPost(HttpServletRequest request,
+	        HttpServletResponse response)
+	        throws ServletException, IOException {
 		
-		
-	}
 
-}
+		
+		}
+	}
