2bc8df18cfa611405e74b19f9363a0020aab8f8d
plusdojo2026-ishidareoto <ishida-reoto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:07:30 +0900

    最新版です。

--- source/src/main/java/dao/Reg_EdiDAO.java
+++ source/src/main/java/dao/Reg_EdiDAO.java
@@ -102,7 +102,7 @@ public class Reg_EdiDAO {
 	            pStmt.setInt(5, dto.getWperiod());
 	            pStmt.setString(6, dto.getMaker());
 	            pStmt.setInt(7, dto.getLife());
-	            pStmt.setDouble(8, dto.getDay_price());     // 仮
+	            pStmt.setDouble(8, dto.getDay_priceInt());     // 仮
 		     	pStmt.setInt(9, dto.getProgress());         // 仮
 		     	pStmt.setInt(10, dto.getGoal());            //　仮  
 	            pStmt.setString(11, dto.getNickname());
