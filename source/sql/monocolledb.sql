e515c9d8c6b39689d13d091941faa1777d91a2a3
plusdojo2026-yamashita <yamashita-yuka-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 16:26:06 +0900

    monocolledb更新

--- source/sql/monocolledb.sql
+++ source/sql/monocolledb.sql
@@ -92,6 +92,42 @@ INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_p
 INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img) 
 	VALUES ('家電', '扇風機', '2026-06-18', 30000, 90, 'アイリスオーヤマ', 5, 30000/(5*365), 3, 200, '風神', NULL);
 	
+	
+	
+/* userid最後尾に追加バージョン */
+/* ジャンル・商品名・購入日・価格・保証期間・メーカー・耐用年数・一日あたり価格・経過日数・目標達成まであと何日か・愛称 */
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES ('家電', '冷蔵庫', '2026-06-12', 10000, 90, 'アイリスオーヤマ', 5, 10000/(5*365), 3, 200, 'レイちゃん', NULL, 1);
+	
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('家電', '掃除機', '2023-09-20', 21000, 3, 'スイープリンク社', 7, 21000/(7*365), 1003, 1552, 'すいすい君', NULL, 2);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES ('家具', 'ソファー', '2022-05-10', 26000, 1, 'コンフォート家具工房', 10, 26000/(10*365), 1502, 2148, '祖父', NULL, 1);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('家電', 'エアコン', '2023-06-01', 48000, 5, 'エアロシステムズ', 10, 48000/(10*365), 1114, 1276, 'カチ山コチ太郎', NULL, 2);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('ぬいぐるみ', 'サメぬい', '2024-12-24', 1600, 0, '縫い工房', 12, 1600/(12*365), 543, 3837, 'トンカチ頭', NULL, 1);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('車', '76', '2022-08-05', 1620000, 5, '東洋ターボ', 12, 1620000/(12*365), 1414, 2966, 'ナナロク', NULL, 2);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('家電', '扇風機', '2024-07-12', 4200, 1, 'エアフロー技研', 6, 4200/(6*365), 708, 1482, '寒太郎', NULL, 1);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('その他', '時計', '2024-09-22', 6800, 1, 'タイムクラフト', 10, 6800/(10*365), 636, 302, 'チックンタックン', NULL, 2);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('家電', 'スマートフォン', '2024-10-08', 26800, 1, 'モバイルリンク', 5, 26800/(5*365), 620, 1205, '相棒', NULL, 1);
+
+INSERT INTO shouhin(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid) 
+	VALUES('日用品', '自転車', '2023-04-12', 19800, 1, 'スピードライン工業', 8, 19800/(8*365), 1130, 1790, 'チャリリン・モンロー', NULL, 2);
+
+	
 /* ファイルパスはエスケープする必要あり(\を二重にする) */
 UPDATE shouhin SET img = LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\kaden_reizouko.png') WHERE id = 1;
 
@@ -131,6 +167,40 @@ INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_pr
 INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img) VALUES ('家電', 'テスター', '2026-06-12', 100000, 90, 'アイリスオーヤマ', 5, 2.0, 3, 200, '試験君', NULL);
 INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img) VALUES ('家電', '電卓機', '2026-06-12', 100000, 90, 'アイリスオーヤマ', 5, 2.0, 3, 200, '計算さん', NULL);
 INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img) VALUES ('家電', 'ガレリアPC', '2026-06-12', 100000, 90, 'アイリスオーヤマ', 5, 2.0, 3, 200, 'ハイスペック', NULL);
+
+
+/* userid最後尾に追加バージョン */
+/* ジャンル・商品名・購入日・価格・保証期間(年)・メーカー・耐用年数・一日あたり価格・経過日数・(目標達成〇✖)・愛称 */
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	 VALUES ('家電', '扇風機', '2026-06-12', 100000, 90, 'アイリスオーヤマ', 5, 10000/(5*365), 200, '風ちゃん', NULL, 1);
+
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家電', '冷蔵庫', '2016-04-12', 105000,5 , 'クールテック', 10, 105000/(10*365), 3950, 3950, 'ひやりん', NULL, 1);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家電', '電子レンジ', '2017-03-09', 29800, 1, 'ヒートウェーブ', 8, 29800/(8*365), 3350, 3350, 'マイク', NULL, 2);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家電', '掃除機', '2016-01-01', 52000, 3, 'クリーンスター', 7, 52000/(3*365), 2002, 2002, 'Mr.スター', NULL, 2);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家電', '洗濯機', '2017-04-10',48000, 3, 'アクアクリン製作所', 8, 48000/(8*365), 3052,  3052,'せんたくん', NULL, 2);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家電', 'トースター', '2018-02-15',6800, 1, 'ベイクテック', 6, 6800/(6*365), 1205,  1205,'パン焼ん', NULL, 1);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家具', 'ソファー', '2015-09-01',32000, 1, 'コンフォート家具工房', 10, 32000/(10*365), 3800,  3800,'ふかふか丸', NULL, 1);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('家電', 'エアコン', '2016-05-20',58000, 5, 'エアロシステムズ', 10, 58000/(10*365), 1944,  1944,'サム', NULL, 2);
+
+INSERT INTO rireki(genre, shouhin, buy_date, price, wperiod, maker, life, day_price, progress, goal, nickname, img, userid)
+	VALUES ('ぬいぐるみ', 'モフモフベア', '2017-12-10',2400, 0 , '縫い工房', 12, 2400/(12*365), 1080 , 1080 ,'もふぃ', NULL, 1);
+
+
+
 
 /* ファイルパスはエスケープする必要あり(\を二重にする) */
 UPDATE rireki SET img = LOAD_FILE('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\kaden_reizouko.png') WHERE id = 1;
